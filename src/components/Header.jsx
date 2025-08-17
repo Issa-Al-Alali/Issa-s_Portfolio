1cfa5099b1dcfd48eb8a379bbab6b0b2fa64aeb1
Issa-Al-Alali <issa.al.alali@gmail.com>
2025-08-17 20:51:35 +0300

    Header responsivity issue fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import './Header.css';
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import "./Header.css";
 
 const Header = () => {
   return (
     <Navbar collapseOnSelect expand="lg" fixed="top">
       <Container>
-        <Navbar.Brand href="#">My Portfolio</Navbar.Brand>
+        <Navbar.Brand href="#home">My Portfolio</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="ms-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#about">About</Nav.Link>
             <Nav.Link href="#skills">Skills</Nav.Link>
